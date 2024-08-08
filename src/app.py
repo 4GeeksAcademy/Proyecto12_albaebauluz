40c69c1a792e8b313688533176139be893a0e7b8
Albaebauluz Alba <albaebauluz@hotmail.com>
2024-08-08 10:43:06 +0000

    Ejercicio finalizado

--- src/app.py
+++ src/app.py
@@ -3,15 +3,14 @@ import pandas as pd
 import seaborn as sns
 from dotenv import load_dotenv
 import spotipy
-from dotenv import load_dotenv
 import os
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
-
+import matplotlib.pyplot as plt
 
 # load the .env file variables
 load_dotenv()
 
-#3) 
+#3) Variables entorno:
 
 client_id = os.environ.get("CLIENT_ID")
 client_secret = os.environ.get("CLIENT_SECRET")
@@ -25,7 +24,6 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
 ))
 
 #5) Realizar solicitudes a la API:
-
 
 client_credential_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret) 
 
@@ -47,6 +45,28 @@ if response:
     popu = track['popularity'] 
     time = int(track['duration_ms'])/(1000*60)
     df=pd.concat([df,pd.DataFrame({'name':name,'popularity':popu,'time':time}, index = [0])], ignore_index = True)
-print(df)
+#print(df)
 
-df_popularity = 
+# 6.1) Ordeno las canciones por popularidad creciente:
+df_popularity = df.sort_values('popularity')
+#print(df_popularity)
+
+#6.2) Muestro el top 3
+#df_popularity.head()
+#df_popularity.nlargest(3, popu)
+
+#print(df[:3])
+
+#7) Análisis estadístico:
+
+#7.1) Gráfico correlación entre popularidad y duración de la canción:
+scatter_plot = sns.scatterplot(data = df, x = 'popularity', y = 'time')
+fig =scatter_plot.get_figure()
+fig.savefig("scatter_plot.png")
+
+plt.show()
+
+"""
+CONCLUSIÓN: 
+El gráfico muestra que, a pesar de que se puede observar una ligera tendencia positiva entre la popularidad de una canción y su duración, no existe suficiente evidencia para afirmar que existe correlación entre  ambas variables. 
+Es necesario analizar una muestra más amplia para poder evaluar la correlación entre variables.
